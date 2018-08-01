9462075ce8d1d8f4025819e55630fe7f15eb46ef
Rishi Pappu <rishi.g.pappu@gmail.com>
2018-08-01 16:38:29 -0700

    Finished teams navigation

--- js/index.js
+++ js/index.js
@@ -7,4 +7,11 @@ function toggleNav() {
 function toggleTeamNav() {
     toggleNav();
     $("body").toggleClass("toggledteam");
+    $(".teamletter").toggleClass("animated");
+    $(".teamletter").toggleClass("slideInLeft");
 }
+
+currentYear = document.getElementById('currentYear');
+var date = new Date();
+var year = date.getFullYear();
+currentYear.innerHTML = year;
